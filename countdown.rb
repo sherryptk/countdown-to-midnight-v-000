b1513431e5b9949de64a2869407bb52daee139d9
Sherry <sherryptk@gmail.com>
2017-11-05 19:31:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,17 @@ def countdown(int)
   end
 
   "HAPPY NEW YEAR!"
-  
+
 end
 
 def countdown_with_sleep
 
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep 1
+  end
 
+  "HAPPY NEW YEAR!"
 
 end
